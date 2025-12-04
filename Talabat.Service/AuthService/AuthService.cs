a5ee0d7f66fb1d25dfc5bc9177534c0cc30e203a
Eslam Ashraf <144673435+ESLAMELSAADANI@users.noreply.github.com>
2025-12-04 17:05:46 +0200

    -Auth Service - JWT Token Creation

---
+++ Talabat.Service/AuthService/AuthService.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities.Identity;
+using Talabat.Core.Services.Contract;
+
+namespace Talabat.Application.AuthService
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IConfiguration _configuration;
+
+        public AuthService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<string> CreateTokenAsync(ApplicationUser user, UserManager<ApplicationUser> userManager)
+        {
+            //1- Header => algorithm and token type => setted when generate the token object JwtSecurityToken()
+            
+            //2- Payload => private & registered claims
+            //Private Claims => User Defined
+            var privateClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name,user.DisplayName),
+                new Claim(ClaimTypes.Email,user.Email),
+            };
+
+            var userRoles = await userManager.GetRolesAsync(user);
+
+            foreach (var role in userRoles)
+            {
+                privateClaims.Add(new Claim(ClaimTypes.Role, role));
+            }
+            
+            //3- Signature
+            //SecreKey
+            var authKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:AuthKey"] ?? string.Empty));
+
+            //token object with registered claims && Private Claims && secretKey
+            var token = new JwtSecurityToken(
+                audience: _configuration["JWT:ValidAudience"],
+                issuer: _configuration["JWT:ValidIssuer"],
+                expires: DateTime.Now.AddDays(double.Parse(_configuration["JWT:DurationInDays"] ?? "0")),
+                claims:privateClaims,
+                signingCredentials:new SigningCredentials(authKey,SecurityAlgorithms.HmacSha256Signature)
+                );
+
+            //Final String Token
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
