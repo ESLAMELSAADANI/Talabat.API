46fc5c9b03aab7c23c2b193cf17bb4b6cfa9dc72
Eslam Ashraf <144673435+ESLAMELSAADANI@users.noreply.github.com>
2025-12-07 19:02:44 +0200

    -Make Order Service [Code Contract(in Core Project) & Make The Class that will implement this interface (in Application Project) ]

---
+++ Talabat.Application/OrderService/OrderService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities.Order_Aggregate;
+using Talabat.Core.Services.Contract;
+
+namespace Talabat.Application.OrderService
+{
+    public class OrderService : IOrderService
+    {
+        public Task<Order> CreateOrderAsync(string buyerEmail, string basketId, Address shippingAddress, string deliveryMethodId)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<Order> GetOrderByIdForUserAsync(string buyerEmail, int orderId)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string buyerEmail)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
