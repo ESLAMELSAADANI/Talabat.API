756cf66f8162e8c56d0f3594bb8b8c60ad8e7a4c
Eslam Ashraf <144673435+ESLAMELSAADANI@users.noreply.github.com>
2025-12-07 13:25:46 +0200

    -Order Module - Entities

---
+++ Talabat.Core/Entities/Order Aggregate/Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities.Order_Aggregate
+{
+    public class Order : BaseEntity
+    {
+        public string BuyerEmail { get; set; } = null!;
+        public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.Now;
+        public OrderStatus Status { get; set; } = OrderStatus.Pending;
+        public Address ShippingAddress { get; set; } = null!;
+        //public int DeliveryMethodId { get; set; }//Foreign Key
+        public DeliveryMethod DeliveryMethod { get; set; } = null!;//Navigational Prop [One]
+        public ICollection<OrderItem> Items { get; set; } = new HashSet<OrderItem>();//Navigational Property [Many]
+        public decimal SubTotal { get; set; }//Cost Of Order With it's all items without shipping cost
+        //[NotMapped]
+        //public decimal Total => SubTotal + DeliveryMethod.Cost;//Read Only Attribute
+        public decimal GetTotal() => SubTotal + DeliveryMethod.Cost;
+        public string PaymentIntentId { get; set; } = string.Empty;//نية الدفع
+    }
+}
---
+++ Talabat.Core/Entities/Order Aggregate/OrderItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities.Order_Aggregate
+{
+    //The product that ordered as an item inside the CustomerOrder with specific price and quantity 
+    public class OrderItem : BaseEntity
+    {
+        public ProductItemOrdered Product { get; set; } = null!;
+        public decimal Price { get; set; }//Price of product as an item in the order not the productPrice - May be there is voucher or discount
+        public int Quantity { get; set; }
+    }
+}
