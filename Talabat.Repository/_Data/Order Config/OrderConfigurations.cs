a3bff1e3e06e4341dd3f4305035fdd946bce519b
Eslam Ashraf <144673435+ESLAMELSAADANI@users.noreply.github.com>
2025-12-07 13:58:18 +0200

    -Order Module => Configuration classes -For [Order - OrderItem - DeliveryMethod] Tables.

---
+++ Talabat.Repository/_Data/Order Config/OrderConfigurations.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Talabat.Core.Entities.Order_Aggregate;
+
+namespace Talabat.Infrastructure._Data.Order_Config
+{
+    public class OrderConfigurations : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.OwnsOne(Order => Order.ShippingAddress, ShippingAddress => ShippingAddress.WithOwner());//Composite Attribute - Not Map the prop [ShippingAddress] as seperate table, map it's properties inside Order table.
+            builder.Property(order => order.Status)
+                   .HasConversion(
+                (OrderStatus) => OrderStatus.ToString(),
+                (OrderStatus) => (OrderStatus)Enum.Parse(typeof(OrderStatus), OrderStatus)
+                );//To Store it as string in DB and when returned returned as OrderStatus integer value
+
+            builder.Property(order => order.SubTotal)
+                   .HasColumnType("decimal(12,2)");
+        }
+    }
+}
