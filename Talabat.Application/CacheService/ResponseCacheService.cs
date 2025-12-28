6d1297a2a6852a7dc2aca3baa4faced91b139f4d
Eslam Ashraf <144673435+ESLAMELSAADANI@users.noreply.github.com>
2025-12-28 14:23:15 +0200

    -Implement the caching Module by make the contract/interface IResponseCaching which contain sognatures for 2 methods: 1- Cache the response in memory [in-memory db redis] with specific time to live 2- get the cached response in redis db by the key

---
+++ Talabat.Application/CacheService/ResponseCacheService.cs
@@ -0,0 +1,39 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Talabat.Core.Services.Contract;
+
+namespace Talabat.Application.CacheService
+{
+    public class ResponseCacheService : IResponnseCacheService
+    {
+        private readonly IDatabase _database;
+
+        public ResponseCacheService(IConnectionMultiplexer redis)
+        {
+            _database = redis.GetDatabase();
+        }
+
+        public async Task CacheResponseAsync(string key, object response, TimeSpan timeToLive)
+        {
+            if (response is null) return;
+
+            var serializeOptions = new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };//When Serialize to json it returned not as camel case and frontend only understand camel case so convert it first
+            var serializedResponse = JsonSerializer.Serialize(response,serializeOptions);
+
+            await _database.StringSetAsync(key, serializedResponse, timeToLive);
+        }
+
+        public async Task<string?> GetCachedResponseAsync(string key)
+        {
+            var response = await _database.StringGetAsync(key);
+
+            if (response.IsNullOrEmpty) return null;
+            return response;
+        }
+    }
+}
